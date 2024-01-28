7b24e7a70fe7d213adafec9828d0a23fe990f9be
hgbeleza <developer.beleza06@gmail.com>
2024-01-28 12:16:02 -0300

    adicionando uma parte do projeto

---
+++ src/app/shared/depoimentos/depoimentos.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-depoimentos',
+  templateUrl: './depoimentos.component.html',
+  styleUrls: ['./depoimentos.component.scss'],
+})
+export class DepoimentosComponent {
+  depoimento: string = `
+    Recomendo fortemente a agência de viagens Jornada.
+    Eles oferecem um serviço personalizado e de alta qualidade
+    que excedeu minhas expectativas em minha última viagem.
+  `;
+  autoria: string = 'Mariana Faustino';
+}
